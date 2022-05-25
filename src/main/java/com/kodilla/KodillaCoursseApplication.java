6b4d3d50dd41e0d9b66d189f48521192af08a84e
zuzannazdeb <zdebzuzanna@gmail.com>
2022-05-25 21:32:09 +0200

    First Commit

---
+++ src/main/java/com/kodilla/KodillaCoursseApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCoursseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCoursseApplication.class, args);
+    }
+
+}
