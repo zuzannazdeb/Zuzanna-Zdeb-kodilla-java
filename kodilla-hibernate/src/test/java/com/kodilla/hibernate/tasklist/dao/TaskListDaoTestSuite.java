cca07e590f066ab267c423c2a5118f8a84d1bad0
zuzannazdeb <zdebzuzanna@gmail.com>
2022-09-14 17:14:04 +0200

    13.2 Tworzymy encje

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("ListName", "Description");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List <TaskList> readTaskLists = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals(1,readTaskLists.size());
+
+        //CleanUp
+        int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
