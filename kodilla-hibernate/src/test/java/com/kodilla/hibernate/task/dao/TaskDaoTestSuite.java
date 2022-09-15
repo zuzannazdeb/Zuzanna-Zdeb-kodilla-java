c576c210573a02d46dfbfab55ef37b9c0523a0e1
zuzannazdeb <zdebzuzanna@gmail.com>
2022-09-15 20:19:16 +0200

    13.2.3 Tworzymy encje

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -3,12 +3,15 @@ package com.kodilla.hibernate.task.dao;
 import com.kodilla.hibernate.task.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@EnableAutoConfiguration(exclude= DataSourceAutoConfiguration.class)
 @SpringBootTest
 class TaskDaoTestSuite {
 
